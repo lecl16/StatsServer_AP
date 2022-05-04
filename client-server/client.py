7fdaae7cf92887e50e619dd2e9b7e3967dc00de7
Luis Leal <lecl@ua.pt>
2022-05-04 19:29:48 +0100

    def create_file

--- client-server/client.py
+++ client-server/client.py
@@ -11,32 +11,33 @@ from Crypto.Cipher import AES
 
 # Função para encriptar valores a enviar em formato json com codificação base64
 # return int data encrypted in a 16 bytes binary string coded in base64
-def encrypt_intvalue (cipherkey, data):
-	cipher = AES.new (cipherkey, AES.MODE_ECB)
-	encripted = cipher.encrypt(bytes("%16" % (data), 'utf-8'))
-	data_tosend = str (base64.b64encode (encripted), "utf-8")
-	return data_tosend
+
+
+def encrypt_intvalue(cipherkey, data):
+    cipher = AES.new(cipherkey, AES.MODE_ECB)
+    encripted = cipher.encrypt(bytes("%16" % (data), 'utf-8'))
+    data_tosend = str(base64.b64encode(encripted), "utf-8")
+    return data_tosend
 
 
 # Função para desencriptar valores recebidos em formato json com codificação base64
 # return int data decrypted from a 16 bytes binary strings coded in base64
-def decrypt_intvalue (cipherkey, data):
-	cipher = AES.new (cipherkey, AES.MODE_ECB)
-	data = base64.b64decode(data)
-	data = cipher.decrypt(data)
-	data = int (str (data, "utf-8"))
-	return None
-
+def decrypt_intvalue(cipherkey, data):
+    cipher = AES.new(cipherkey, AES.MODE_ECB)
+    data = base64.b64decode(data)
+    data = cipher.decrypt(data)
+    data = int(str(data, "utf-8"))
+    return None
 
 
 # verify if response from server is valid or is an error message and act accordingly
-def validate_response (client_sock, response):
-	return None
+def validate_response(client_sock, response):
+    return None
 
 
 # process QUIT operation
-def quit_action (client_sock, attempts):
-	return None
+def quit_action(client_sock, attempts):
+    return None
 
 
 # Outcomming message structure:
@@ -55,28 +56,29 @@ def quit_action (client_sock, attempts):
 #
 # Suporte da execução do cliente
 #
-def run_client (client_sock, client_id):
-	return None
-	
+def run_client(client_sock, client_id):
+    return None
+
 
 def main():
-	# validate the number of arguments and eventually print error message and exit with error
-	# verify type of of arguments and eventually print error message and exit with error
+    # validate the number of arguments and eventually print error message and exit with error
+    # verify type of of arguments and eventually print error message and exit with error
 
-	cipherkey = os.urandom(16)
-	cipherkey_tosend = str (base64.b64encode (cipherkey), "utf8")
-	cipher = AES.new (cipherkey, AES.MODE_ECB)
+    cipherkey = os.urandom(16)
+    cipherkey_tosend = str(base64.b64encode(cipherkey), "utf8")
+    cipher = AES.new(cipherkey, AES.MODE_ECB)
 
-	port = ?
-	hostname = ?
+    port = ?
+    hostname = ?
 
-	client_sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-	client_sock.connect ((hostname, port))
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.connect((hostname, port))
 
-	run_client (client_sock, sys.argv[1])
+    run_client(client_sock, sys.argv[1])
 
-	client_sock.close ()
-	sys.exit (0)
+    client_sock.close()
+    sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
--- client-server/server.py
+++ client-server/server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from email import header
+import re
 import sys
 import socket
 import select
@@ -7,6 +9,7 @@ import json
 import base64
 import csv
 import random
+from os import path
 from common_comm import send_dict, recv_dict, sendrecv_dict
 
 from Crypto.Cipher import AES
@@ -15,31 +18,33 @@ from Crypto.Cipher import AES
 users = {}
 
 # return the client_id of a socket or None
-def find_client_id (client_sock):
-	return None
+
+
+def find_client_id(client_sock):
+    return None
 
 
 # Função para encriptar valores a enviar em formato json com codificação base64
 # return int data encrypted in a 16 bytes binary string and coded base64
-def encrypt_intvalue (client_id, data):
-	#INCOMPLETO
-	#
-	cipher = AES.new(cipherkey, AES.MODE_ECB)
-	data2 = cipher.encrypt(bytes("%16d" % (data), 'utf8'))
-	data_tosend = str(base64.b64encode(data2), 'utf8')
-	return data_tosend
+def encrypt_intvalue(client_id, data):
+    # INCOMPLETO
+    #
+    cipher = AES.new(cipherkey, AES.MODE_ECB)
+    data2 = cipher.encrypt(bytes("%16d" % (data), 'utf8'))
+    data_tosend = str(base64.b64encode(data2), 'utf8')
+    return data_tosend
 
 
 # Função para desencriptar valores recebidos em formato json com codificação base64
 # return int data decrypted from a 16 bytes binary string and coded base64
-def decrypt_intvalue (client_id, data):
-	#INCOMPLETO
-	#
-	cipher = AES.new(cipherkey, AES.MODE_ECB)
-	data = base64.b64decode(data)
-	data = cipher.decrypt(data)
-	data = int(str(data, 'utf8'))
-	return data
+def decrypt_intvalue(client_id, data):
+    # INCOMPLETO
+    #
+    cipher = AES.new(cipherkey, AES.MODE_ECB)
+    data = base64.b64decode(data)
+    data = cipher.decrypt(data)
+    data = int(str(data, 'utf8'))
+    return data
 
 
 # Incomming message structure:
@@ -58,8 +63,18 @@ def decrypt_intvalue (client_id, data):
 #
 # Suporte de descodificação da operação pretendida pelo cliente
 #
-def new_msg (client_sock):
-	return None
+def new_msg(client_sock):
+    request = recv_dict(client_sock)
+    # print(request)
+    if request['op'] == "START":
+        new_client(client_sock, request)
+    if request['op'] == "QUIT":
+        quit_client(client_sock, request)
+    if request['op'] == "STOP":
+        stop_client(client_sock, request)
+    if request['op'] == "NUMBER":
+        number_client(client_sock, request)
+    return None
 # read the client request
 # detect the operation requested by the client
 # execute the operation and obtain the response (consider also operations not available)
@@ -69,8 +84,8 @@ def new_msg (client_sock):
 #
 # Suporte da criação de um novo jogador - operação START
 #
-def new_client (client_sock, request):
-	return None
+def new_client(client_sock, request):
+    return None
 # detect the client in the request
 # verify the appropriate conditions for executing this operation
 # process the client in the dictionary
@@ -80,16 +95,16 @@ def new_client (client_sock, request):
 #
 # Suporte da eliminação de um cliente
 #
-def clean_client (client_sock):
-	return None
+def clean_client(client_sock):
+    return None
 # obtain the client_id from his socket and delete from the dictionary
 
 
 #
 # Suporte do pedido de desistência de um cliente - operação QUIT
 #
-def quit_client (client_sock, request):
-	return None
+def quit_client(client_sock, request):
+    return None
 # obtain the client_id from his socket
 # verify the appropriate conditions for executing this operation
 # process the report file with the QUIT result
@@ -100,24 +115,28 @@ def quit_client (client_sock, request):
 #
 # Suporte da criação de um ficheiro csv com o respectivo cabeçalho
 #
-def create_file ():
-	return None
+def create_file():
+    if path.exists("report.csv", "w") == False:
+        with open("report.csv", "w") as csvFile:
+            w = csv.DictWriter(csvFile, fieldnames=header)
+            w.writeheader()
+    return None
 # create report csv file with header
 
 
 #
 # Suporte da actualização de um ficheiro csv com a informação do cliente e resultado
 #
-def update_file (client_id, result):
-	return None
+def update_file(client_id, result):
+    return None
 # update report csv file with the result from the client
 
 
 #
 # Suporte do processamento do número de um cliente - operação NUMBER
 #
-def number_client (client_sock, request):
-	return None
+def number_client(client_sock, request):
+    return None
 # obtain the client_id from his socket
 # verify the appropriate conditions for executing this operation
 # return response message with or without error message
@@ -126,8 +145,8 @@ def number_client (client_sock, request):
 #
 # Suporte do pedido de terminação de um cliente - operação STOP
 #
-def stop_client (client_sock, request):
-	return None
+def stop_client(client_sock, request):
+    return None
 # obtain the client_id from his socket
 # verify the appropriate conditions for executing this operation
 # process the report file with the result
@@ -136,44 +155,46 @@ def stop_client (client_sock, request):
 
 
 def main():
-	# validate the number of arguments and eventually print error message and exit with error
-	# verify type of of arguments and eventually print error message and exit with error
-	
-	port = ?
+    # validate the number of arguments and eventually print error message and exit with error
+    # verify type of of arguments and eventually print error message and exit with error
 
-	server_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind (("127.0.0.1", port))
-	server_socket.listen (10)
+    port = int(sys.argv[2])
 
-	clients = []
-	create_file ()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", port))
+    server_socket.listen(10)
 
-	while True:
-		try:
-			available = select.select ([server_socket] + clients, [], [])[0]
-		except ValueError:
-			# Sockets may have been closed, check for that
-			for client_sock in clients:
-				if client_sock.fileno () == -1: client_sock.remove (client) # closed
-			continue # Reiterate select
+    clients = []
+    create_file()
 
-		for client_sock in available:
-			# New client?
-			if client_sock is server_socket:
-				newclient, addr = server_socket.accept ()
-				clients.append (newclient)
-			# Or an existing client
-			else:
-				# See if client sent a message
-				if len (client_sock.recv (1, socket.MSG_PEEK)) != 0:
-					# client socket has a message
-					##print ("server" + str (client_sock))
-					new_msg (client_sock)
-				else: # Or just disconnected
-					clients.remove (client_sock)
-					clean_client (client_sock)
-					client_sock.close ()
-					break # Reiterate select
+    while True:
+        try:
+            available = select.select([server_socket] + clients, [], [])[0]
+        except ValueError:
+            # Sockets may have been closed, check for that
+            for client_sock in clients:
+                if client_sock.fileno() == -1:
+                    client_sock.remove(client)  # closed
+            continue  # Reiterate select
+
+        for client_sock in available:
+            # New client?
+            if client_sock is server_socket:
+                newclient, addr = server_socket.accept()
+                clients.append(newclient)
+            # Or an existing client
+            else:
+                # See if client sent a message
+                if len(client_sock.recv(1, socket.MSG_PEEK)) != 0:
+                    # client socket has a message
+                    ##print ("server" + str (client_sock))
+                    new_msg(client_sock)
+                else:  # Or just disconnected
+                    clients.remove(client_sock)
+                    clean_client(client_sock)
+                    client_sock.close()
+                    break  # Reiterate select
+
 
 if __name__ == "__main__":
-	main()
+    main()
